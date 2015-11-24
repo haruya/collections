9bec1400484ddceba6c1baaa1862e6d62b7b87ab
Haruya Ichikawa <ichikawa0210h@gmail.com>
2015-11-25 01:27:47 +0900

    first commit

---
+++ app/Http/Controllers/SetupController.php
@@ -0,0 +1,66 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Http\Request;
+
+class SetupController extends Controller {
+
+	public function getIndex()
+	{
+		return view('setup.index');
+	}
+
+	public function getCreate($db_table)
+	{
+		// テーブルの存在確認
+		if (Schema::hasTable($db_table)) {
+			return redirect()
+			->back()
+			->withMessage("{$db_table}テーブルが存在しますので、処理を中断します。")
+			->withAlert("alert alert-danger");
+		}
+		if ($db_table == 'gathers') {
+			Schema::create($db_table, function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->string('name',100);
+				$table->integer('category_id')->index();
+				$table->string('code',100)->index();
+				$table->text('description')->nullable();
+				$table->timestamps();
+				$table->timestamp('deleted_at',null)->nullable();
+			});
+			return redirect()
+			->back()
+			->withMessage("{$db_table}テーブルを作成しました")
+			->withAlert("alert alert-success");
+		} elseif ($db_table == 'categories') {
+			Schema::create($db_table, function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->string('name');
+				$table->text('description')->nullable();
+			});
+			return redirect()
+			->back()
+			->withMessage("{$db_table}テーブルを作成しました")
+			->withAlert("alert alert-success");
+		} else {
+			Schema::create($db_table, function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->string('name');
+				$table->text('description')->nullable();
+				$table->timestamps();
+				$table->timestamp('deleted_at',null)->nullable();
+			});
+			return redirect()
+			->back()
+			->withMessage("{$db_table}テーブルを作成しました")
+			->withAlert("alert alert-success");
+		}
+	}
+}
---
+++ resources/views/setup/index.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+@section('content')
+<div class="container">
+<h2>テーブルの作成</h2>
+@if(Session::has('message'))
+<p class="{{Session::get('alert')}}">
+{{Session::get('message')}}
+</p>
+@endif
+
+<a href="{{url('setup/create/gathers')}}" class="btn btn-primary btn-lg btn-block">gathersテーブルの作成</a>
+<a href="{{url('setup/create/categories')}}" class="btn btn-primary btn-lg btn-block">categoriesテーブルの作成</a>
+</div>
+@stop
